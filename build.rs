675bab45f3db203e5a193c2aeb05e627458f072e
Josh Philpott <73914347+jphilpott0@users.noreply.github.com>
2026-01-22 01:49:57 +0000

    Start handwritten block init stage

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
-const FILES: [&str; 3] = [
+const FILES: &[&str] = &[
     "arch/x86_64/len.S",
     "arch/x86_64/arena.S",
     "arch/x86_64/speq.S",
+    "arch/x86_64/cell.S",
 ];
 
 fn main() {
@@ -9,7 +10,7 @@ fn main() {
         println!("cargo:rerun-if-changed={file}");
     }
 
-    nasm_rs::compile_library("libfasted.a", &FILES).unwrap();
+    nasm_rs::compile_library("libfasted.a", FILES).unwrap();
 
     let out_dir = std::env::var("OUT_DIR").expect("Could not find env var OUT_DIR");
 
